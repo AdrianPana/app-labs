d86c3f22be9571a49fb2fb3e0821ff3cfa88aa58
iminblue <panaadry@yahoo.com>
2024-10-09 13:04:23 +0300

    lab1

--- lab1/skel/sum_array.c
+++ lab1/skel/sum_array.c
@@ -7,16 +7,26 @@ int main() {
 
     int a[SIZE], i;
     long sum = 0;
+    long sum2 = 0;
     
+    #pragma omp parallel for private(i) shared(a)
     for (i = 0; i < SIZE; i++) {
         a[i] = i;
     }
 
+    #pragma omp parallel for reduction(+:sum) private(i)
     for (i = 0; i < SIZE; i++) {
         sum += a[i];
     }
 
+    #pragma omp parallel for shared(sum2)
+    for (i = 0; i < SIZE; i++) {
+        #pragma omp critical
+        sum2 += i;
+    }
+
     printf("Total sum = %ld\n", sum);
+    printf("Total sum2 = %ld\n", sum2);
 
     return 0;
 }
